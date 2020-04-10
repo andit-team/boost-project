5068d970dc66a6cb92842eba94d8191eb42b373a
k.m.jalal hossain <jalal.khan981@gmail.com>
2020-04-09 19:09:27 -0800

    solve some error

--- app/Models/BuyerBillingAddress.php
+++ app/Models/BuyerBillingAddress.php
@@ -11,8 +11,10 @@ class BuyerBillingAddress extends Model
 {
    protected $fillable = ['location','address','country','state','city','zip_code','phone','fax','active','buyer_id','user_id'];
 
-   public function user(){
-    return $this->belongsTo(User::class,'user_id');
+   public function user()
+   {
+       return $this->belongsTo(User::class, 'user_id');
+   }
 
     public function buyer(){
      return $this->belongsTo(Buyer::class,'buyer_id');
