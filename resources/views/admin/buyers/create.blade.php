22856f8b30047b033f377be9df720156417ea308
k.m.jalal hossain <jalal.khan981@gmail.com>
2020-04-20 18:57:13 -0800

    profile data save into data base

--- resources/views/admin/buyers/create.blade.php
+++ resources/views/admin/buyers/create.blade.php
@@ -26,12 +26,11 @@
     <!-- section start -->
     <section class="section-b-space">
         <div class="container">
-            <div class="row">
+            <section class="row">
                 <div class="col-lg-3">
                     <div class="account-sidebar"><a class="popup-btn">my account</a></div>
                     <div class="dashboard-left">
-                        <div class="collection-mobile-back"><span class="filter-back"><i class="fa fa-angle-left"
-                                                                                         aria-hidden="true"></i> back</span></div>
+                        <div class="collection-mobile-back"><span class="filter-back"><i class="fa fa-angle-left" aria-hidden="true"></i> back</span></div>
                         <div class="block-content">
                             <ul>
                                 <li class="active"><a href="#">Account Info</a></li>
@@ -53,36 +52,49 @@
 
                 <div class="col-sm-9 register-page contact-page container">
                     <h3>PERSONAL DETAIL</h3>
-                    <form class="theme-form">
+                    <form class="theme-form" action="{{ route('buyer.store') }}" method="post" enctype="multipart/form-data">
+                        @csrf
                         <div class="form-row">
                             <div class="col-md-6">
-                                <label for="name">First Name</label>
-                                <input type="text" class="form-control" id="name" placeholder="Enter Your name"
-                                       required="">
+                                <label for="full_name">Full Name</label>
+                                <input type="text" class="form-control" name="full_name"   placeholder="Enter Your name" required="">
+                                @if ($errors->has('full_name'))
+                                    <span class="text-danger">{{ $errors->first('full_name') }}</span>
+                                @endif
                             </div>
                             <div class="col-md-6">
-                                <label for="email">Last Name</label>
-                                <input type="text" class="form-control" id="last-name" placeholder="Email" required="">
+                                <label for="phone_number">Phone number</label>
+                                <input type="number" class="form-control" name="phone_number" maxlength="11" minlength="11" id="review" placeholder="Enter your number">
                             </div>
                             <div class="col-md-6">
-                                <label for="review">Phone number</label>
-                                <input type="text" class="form-control" id="review" placeholder="Enter your number"
-                                       required="">
+                                <label for="dob">Date of birth</label>
+                                <input type="text" class="form-control datepicker"  name="dob" id="" placeholder="" >
                             </div>
                             <div class="col-md-6">
-                                <label for="email">Email</label>
-                                <input type="text" class="form-control" id="email" placeholder="Email" required="">
+                                <label for="picture">Picture</label>
+                                <input type="file" class="form-control" name="picture" id="" placeholder="" >
+                            </div>
+                            <div class="col-md-6">
+                                <input name="gender" value="Male" type="radio" class="with-gap" id="Male">
+                                <label for="Male" class="col-md-2">Male</label>
+                                <input name="gender" value="Female" type="radio" id="Female" class="with-gap" checked>
+                                <label for="Female" class="col-md-2">Female</label>
+                                <input name="gender" value="Other" type="radio" id="Other" class="with-gap">
+                                <label for="Other" class="col-md-2">Other</label>
                             </div>
                             <div class="col-md-12">
                                 <label for="review">Write Your Message</label>
-                                <textarea class="form-control mb-0" placeholder="Write Your Message"
-                                          id="exampleFormControlTextarea1" rows="6"></textarea>
+                                <textarea class="form-control mb-0" placeholder="Write Your Message" name="description" id="exampleFormControlTextarea1" rows="6"></textarea>
                             </div>
                             <div class="col-md-12 mt-4">
-                                <button class="btn btn-sm btn-solid" type="submit">Save setting</button>
+                                <button type="submit" class="btn btn-sm btn-solid" >Save setting</button>
                             </div>
                         </div>
                     </form>
                 </div>
+            </section>
+        </div>
+    </section>
     <!-- section end -->
 @endsection
+
