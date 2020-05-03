3e8a4e7de438a424eb1d4e1906c654990644a2a3
jalalkhan1011 <jalal.khan981@gmail.com>
2020-05-03 10:09:19 +0600

    change item controller

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -8,7 +8,6 @@ use App\Models\Category;
 use App\Models\Item;
 use App\Models\Size;
 use App\Models\Color;
-use App\Models\Seller;
 use Sentinel;
 use Session;
 use Baazar;
@@ -54,8 +53,6 @@ class ItemsController extends Controller
      */
     public function store(Request $request)
     {
-      $sellerId = Seller::where('user_id',Sentinel::getUser()->id)->first();
-      //dd($sellerId);
       $this->validateForm($request);
       $slug = Baazar::getUniqueSlug($item,$request->name);
         $data = [
@@ -86,7 +83,7 @@ class ItemsController extends Controller
             'category_id' => $request->category_id,
             'size_id' => $request->size_id,
             'color_id' => $request->color_id,
-            'user_id' => $sellerId,
+            // 'user_id' => Sentinel::getUser()->id,
             'created_at' => now(),
         ];
 
