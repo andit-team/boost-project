e1901d28ccb3ac049c81bfea62ddb1bf1d11dfc2
jalalkhan1011 <jalal.khan981@gmail.com>
2020-05-04 15:17:53 +0600

    admin product approvement

--- resources/views/admin/product/show.blade.php
+++ resources/views/admin/product/show.blade.php
@@ -1,10 +1,7 @@
 @extends('admin.layout.master')
 
 @section('content')
-<style>
-    .ml{
-        margin-left: -475px;
-    }
+<style> 
     .imagestyle{
         width: 100px;
         height: 100px;
@@ -16,111 +13,10 @@
     <div class="page-body">
 
         <!-- Container-fluid starts-->
-        <div class="container-fluid">
-            <div class="page-header">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="page-header-left">
-                            <h3>Show Product
-                                <small>AndBaazar Admin panel</small>
-                            </h3>
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <ol class="breadcrumb pull-right">
-                            <li class="breadcrumb-item"><a href="index.html"><i data-feather="home"></i></a></li>
-                            <li class="breadcrumb-item">Product </li>
-                            <li class="breadcrumb-item active">Product</li>
-                        </ol>
-                    </div>
-                </div>
-            </div>
-        </div>
+       
         <!-- Container-fluid Ends-->
 
-        <!-- Container-fluid starts-->
-        <div class="container-fluid">
-            <div class="card tab2-card">
-                <div class="card-header">
-                    <h5>Show Product</h5>
-                </div>
-                <div class="card-body">
-
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <div class="form-group row">
-                                    <label for="validationCustom0" class="col-xl-3 col-md-4">Name <span>*</span></label>
-                                    <input class="form-control col-md-8" name="name" readonly value="{{ $product->name }}" id="validationCustom0" type="text" required="">
-                                </div>
-                                <div class="form-group row">
-                                    <label for="validationCustom0" class="col-xl-3 col-md-4">Image <span>*</span></label> 
-                                    <input type="file" class="form-control col-md-8" readonly name="image" id="image" onchange="loadFile(event)">
-                                    <input type="hidden" value="{{$product->image}}" name="old_image">
-                                </div>
-                                <div class="form-group row">
-                                    <label for="validationCustom0" class="col-xl-3 col-md-4">category <span>*</span></label>
-                                    <input class="form-control col-md-8" name="name" readonly value="{{ $product->category->name }}" id="validationCustom0" type="text" required="">
-                                </div>
-                                <div class="form-group row">
-                                    <label for="validationCustom0" class="col-xl-3 col-md-4">Model No <span>*</span></label>
-                                    <input class="form-control col-md-8" name="name" readonly value="{{ $product->model_no }}" id="validationCustom0" type="text" required="">
-                                </div>
-                                <div class="form-group row">
-                                    <label for="validationCustom0" class="col-xl-3 col-md-4">Made In <span>*</span></label>
-                                    <input class="form-control col-md-8" name="name" readonly value="{{ $product->made_in }}" id="validationCustom0" type="text" required="">
-                                </div>
-                                <div class="form-group row">
-                                    <label for="validationCustom0" class="col-xl-3 col-md-4">Materials <span>*</span></label>
-                                    <input class="form-control col-md-8" name="name" readonly value="{{ $product->materials }}" id="validationCustom0" type="text" required="">
-                                </div>
-                                <div class="form-group row">
-                                    <label for="validationCustom0" class="col-xl-3 col-md-4">Orginal Price <span>*</span></label>
-                                    <input class="form-control col-md-8" name="name" readonly value="{{ $product->org_price }}" id="validationCustom0" type="text" required="">
-                                </div>
-                                <div class="form-group row">
-                                    <label for="validationCustom0" class="col-xl-3 col-md-4">Price <span>*</span></label>
-                                    <input class="form-control col-md-8" name="name" readonly value="{{ $product->price }}" id="validationCustom0" type="text" required="">
-                                </div>
-                                <div class="form-group row">
-                                    <label for="validationCustom0" class="col-xl-3 col-md-4">Label <span>*</span></label>
-                                    <input class="form-control col-md-8" name="name" readonly value="{{ $product->labeled }}" id="validationCustom0" type="text" required="">
-                                </div>
-                                <div class="form-group row">
-                                    <label for="validationCustom0" class="col-xl-3 col-md-4">Video Url  <span>*</span></label>
-                                    <input class="form-control col-md-8" name="name" readonly value="{{ $product->video_url }}" id="validationCustom0" type="text" required="">
-                                </div>
-                                <div class="form-group row">
-                                    <label for="validationCustom0" class="col-xl-3 col-md-4">Minum Order  <span>*</span></label>
-                                    <input class="form-control col-md-8" name="name" readonly value="{{ $product->min_order }}" id="validationCustom0" type="text" required="">
-                                </div>
-                                <div class="form-group row">
-                                    <label for="validationCustom0" class="col-xl-3 col-md-4">Total Order Quanty  <span>*</span></label>
-                                    <input class="form-control col-md-8" name="name" readonly value="{{ $product->total_order_qty }}" id="validationCustom0" type="text" required="">
-                                </div>
-                                <div class="form-group row">
-                                    <label for="validationCustom0" class="col-xl-3 col-md-4">Pack Id  <span>*</span></label>
-                                    <input class="form-control col-md-8" name="name" readonly value="{{ $product->pack_id }}" id="validationCustom0" type="text" required="">
-                                </div>
-                                <div class="form-group row">
-                                    <label class="col-xl-3 col-md-4"></label>
-                                    <div class="checkbox checkbox-primary col-md-4">
-                                        <a href="{{ url('merchant/product/adminIndex') }}"  class="btn btn-primary">Back</a> 
-                                    </div>
-                                    <div class="checkbox checkbox-primary col-md-4 ml">
-                                    <form action="{{ url('merchant/product/approvement/'.$product->slug)}}" method="post" style="margin-top:-2px" id="deleteButton({{ $product->id }})">
-                                            @csrf
-                                            <button type="submit" class="btn btn-warning" onclick="sweetalertDelete({{ $product->id }})">Approve</button>
-                                    </form>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-
-                </div>
-            </div>
-        </div>
-        
+        <!-- Container-fluid starts-->    
     <div class="col-lg-12 col-md-12">
         <div class="card">
             <div class="card-body">
@@ -189,19 +85,31 @@
                                 <td>:</td>
                                 <td>{{$product->total_order_qty}}</td>
                             </tr>
-                            {{--<tr>
-                                <td>Address</td>
+                            <tr>
+                                <td>Pack Id</td>
                                 <td>:</td>
-                                <td>{{$doctor->address}}</td>
+                                <td>{{$product->pack_id}}</td>
                             </tr>
-                        </table> --}}
-                        {{-- <a href="{{url('doctor/'.$doctor->id.'/edit')}}" class="btn bg-theme text-white">Edit</a> --}}
-                        {{-- <a href="{{route('doctor.index')}}" class="btn bg-theme text-white">{{__('messages.back')}}</a> --}}
+                        </table>
+                        
                     </div>
                 </div>
             </div>
-        </div>
+            <div class="form-group row" style="margin-left: 200px;">
+                <label class="col-xl-3 col-md-4"></label>
+                <div class="checkbox checkbox-primary col-md-4">
+                    <a href="{{ url('merchant/product/adminIndex') }}"  class="btn btn-primary">Back</a> 
+                </div>
+                <div class="checkbox checkbox-primary col-md-4" style="margin-left: -400px">
+                <form action="{{ url('merchant/product/approvement/'.$product->slug)}}" method="post" style="margin-top:-2px" id="deleteButton({{ $product->id }})">
+                        @csrf
+                        <button type="submit" class="btn btn-warning" onclick="sweetalertDelete({{ $product->id }})">Approve</button>
+                </form>
+                </div>
+            </div>
+        </div> 
     </div>
+    
         <!-- Container-fluid Ends-->
 
     {{-- </div> --}}
