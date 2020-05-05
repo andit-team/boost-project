54f34c9ecbf0167c9f53a6fa0145eec8eba5f9b3
jalalkhan1011 <jalal.khan981@gmail.com>
2020-05-05 11:33:37 +0600

    frontend templting running

---
+++ resources/views/frontend/include/homeslider.blade.php
@@ -0,0 +1,41 @@
+
+    <section class="p-0">
+        <div class="slide-1 home-slider">
+            <div>
+                <div class="home  text-center">
+                    <img src="{{asset('frontend')}}/assets/images/home-banner/1.jpg" alt="" class="bg-img blur-up lazyload">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col">
+                                <div class="slider-contain">
+                                    <div>
+                                        <h4>welcome to fashion</h4>
+                                        <h1>men fashion</h1>
+                                        <a href="#" class="btn btn-solid">shop now</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="home text-center">
+                    <img src="{{asset('frontend')}}/assets/images/home-banner/2.jpg" alt="" class="bg-img blur-up lazyload">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col">
+                                <div class="slider-contain">
+                                    <div>
+                                        <h4>welcome to fashion</h4>
+                                        <h1>women fashion</h1>
+                                        <a href="#" class="btn btn-solid">shop now</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
