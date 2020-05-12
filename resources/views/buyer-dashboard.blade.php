a1d612b7cd2a9916e1f634411ad1af6e4167b494
rupaik <rupaik.cse@gmail.com>
2020-05-12 16:53:48 +0600

    Pull

--- resources/views/buyer-dashboard.blade.php
+++ resources/views/buyer-dashboard.blade.php
@@ -16,7 +16,7 @@
 <section class="section-b-space">
     <div class="container">
         <div class="row">
-            @include('layouts.inc.sidebar.buyer-sidebar')
+            @include('layouts.inc.sidebar.buyer-sidebar',[$active='dashboard'])
             <div class="col-lg-9">
                 <div class="dashboard-right">
                     <div class="dashboard">
