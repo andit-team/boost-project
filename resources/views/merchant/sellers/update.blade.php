38841dac1b29e3dd5fae4727a5b2ec2764c15608
jalalkhan1011 <jalal.khan981@gmail.com>
2020-05-31 10:22:39 +0600

    reject message show,button fix,email template

--- resources/views/merchant/sellers/update.blade.php
+++ resources/views/merchant/sellers/update.blade.php
@@ -60,6 +60,8 @@
                         <h3 class="card-header text-danger">Seller profile Status</h3>
                             <div class="card-body text-center">
                                 <h4>Your profile is Rejected</h4>
+                                <h6>Reject Reason : <small>{{ $sellerProfile->rej_desc }}</small></h6> 
+                                <p>Resubmit your Profile.</p> 
                             <a href="{{ url('merchant/seller/'.$sellerProfile->slug.'/resubmit') }}" title="Resubmit" class="btn btn-sm btn-solid">Resubmit</a>
                             </div>
                     </div>    
