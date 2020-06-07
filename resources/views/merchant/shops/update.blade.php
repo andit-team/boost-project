e7cf2fd88b7ca48db4a4890c21d0d88fda0cf4cd
Andit@Shariful <webguru071@gmail.com>
2020-06-07 12:59:27 +0600

    take a pull

--- resources/views/merchant/shops/update.blade.php
+++ resources/views/merchant/shops/update.blade.php
@@ -816,36 +816,5 @@ html, body {
     };
 </script>
 
-<script>
-    var marker;
 
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 13,
-    center: {lat: 59.325, lng: 18.070}
-  });
-
-  marker = new google.maps.Marker({
-    map: map,
-    draggable: true,
-    animation: google.maps.Animation.DROP,
-    position: {lat: 59.327, lng: 18.067}
-  });
-  marker.addListener('click', toggleBounce);
-}
-
-function toggleBounce() {
-  if (marker.getAnimation() !== null) {
-    marker.setAnimation(null);
-  } else {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
-  }
-}
-</script>
-
-{{-- <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBLCE9-x9OVyUottiBHi_L6UZKB2rvj7eo&callback=initMap"
-type="text/javascript"></script> --}}
-{{--
-  <script async defer src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
-  type="text/javascript"></script> --}}
 @endpush
