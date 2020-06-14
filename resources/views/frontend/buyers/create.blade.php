8d55ac7f84604a16d77f47bd20438df40d8c8e82
rupaik <rupaik.cse@gmail.com>
2020-06-14 15:28:49 +0600

    Design vendor accept profile email format

--- resources/views/frontend/buyers/create.blade.php
+++ resources/views/frontend/buyers/create.blade.php
@@ -121,55 +121,5 @@
         output.src = URL.createObjectURL(event.target.files[0]);
     };
 </script>
-<script>
-$('#file-upload').croppie(opts);
-// call a method via jquery
-$('#file-upload').croppie(method, args);
-</script>
-
-<script>
-    $('#userpic').fileapi({
-   url: 'http://rubaxa.org/FileAPI/server/ctrl.php',
-   accept: 'image/*',
-   imageSize: { minWidth: 200, minHeight: 200 },
-   elements: {
-      active: { show: '.js-upload', hide: '.js-browse' },
-      preview: {
-         el: '.js-preview',
-         width: 200,
-         height: 200
-      },
-      progress: '.js-progress'
-   },
-   onSelect: function (evt, ui){
-      var file = ui.files[0];
-      if( !FileAPI.support.transform ) {
-         alert('Your browser does not support Flash :(');
-      }
-      else if( file ){
-         $('#popup').modal({
-            closeOnEsc: true,
-            closeOnOverlayClick: false,
-            onOpen: function (overlay){
-               $(overlay).on('click', '.js-upload', function (){
-                  $.modal().close();
-                  $('#userpic').fileapi('upload');
-               });
-               $('.js-img', overlay).cropper({
-                  file: file,
-                  bgColor: '#fff',
-                  maxSize: [$(window).width()-100, $(window).height()-100],
-                  minSize: [200, 200],
-                  selection: '90%',
-                  onSelect: function (coords){
-                     $('#userpic').fileapi('crop', file, coords);
-                  }
-               });
-            }
-         }).open();
-      }
-   }
-});
-</script>
 @endpush
 
