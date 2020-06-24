f47d029da80cac6d825e9e4ae0f950e0b38f056b
rupaik <rupaik.cse@gmail.com>
2020-06-24 16:35:19 +0600

    Attribute insert And Type value insert running

--- resources/views/admin/categories/attribute.blade.php
+++ resources/views/admin/categories/attribute.blade.php
@@ -125,46 +125,7 @@
                                 <td><span class="btn btn-primary rowRemove"><i class="fa fa-trash"></i></span></td>                                    
                                     </tr>
                             </tbody>
-                        </table>
-                            {{-- <div class="form-group">
-                                <label for="category">Label Name:</label>
-                                <input type="text"  name="label" value="{{ old('label') }}" required class="form-control @error('label') border-danger @enderror"> 
-                                <span class="text-danger">{{ $errors->first('label') }}</span>
-                            </div> --}}
-                            {{-- <div class="form-group ">
-                                <label for="category" >Attribute Type:</label>
-                                <select class="form-control"  name="type">
-                                <option value="">Select Attribute Type </option> 
-                                <option id="1" value="multi-select">multi-select </option>
-                                <option id="2" value="select">select </option>
-                                <option id="3" value="text">text </option>
-                                <option id="4" value="checkbox">checkbox </option>
-                                <option id="5" value="radio">radio </option>
-                                <option id="6" value="number">number </option>                                 
-                                </select>
-                            </div>                                                 --}}
-                            {{-- <div class="form-group ">
-                                <label for="percentage">Type Value:</label>                                  
-                                <textarea   name="values"  class="form-control @error('values') border-danger @enderror" rows="5"> </textarea>
-                                <span class="text-danger">{{ $errors->first('values') }}</span>
-                                @foreach($category as $cat)
-                                <input name="category_id" type="hidden" value="{{$cat->slug}}">
-                                @endforeach --}}
-                                {{-- {!! Form::hidden('category_id',  $category->category_id) !!} 
-                            {{-- </div>  --}}
-                            {{-- <input name="category_id" type="hidden" value="2">  --}}                           
-                            {{-- <div class="form-group ">
-                                <label for="percentage">Required:</label>                                  
-                                <div class="form-check form-check-inline">
-                                    <input name="required" value="1" type="checkbox" class="with-gap" id="required" {{$category->required == '1' ? 'checked' : ''}}>
-                                    <input class="form-check-input" type="checkbox" name="required" id="required" value="1">                                    
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="desc">Suggession:</label>
-                                <textarea   name="suggestion"  class="form-control @error('suggestion') border-danger @enderror" rows="5"> </textarea>
-                                <span class="text-danger">{{ $errors->first('suggestion') }}</span>
-                            </div>    --}}
+                        </table>                         
                             <div class="text-right">
                                 <button type="submit" class="btn btn-secondary">Save</button>
                             </div>
