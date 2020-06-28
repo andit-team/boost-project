fb55e933c23cf80790805cfc4ed6929edd0b120c
Rupa Paik <rupapaik@gmail.com>
2020-06-28 14:39:36 +0600

    collect home appliance second chils and seed data

--- database/seeds/CategoriesAttr/Home_Appliance.php
+++ database/seeds/CategoriesAttr/Home_Appliance.php
@@ -5,15 +5,125 @@ $home_appliances = [
 	[
         'Home Appliance',
         'child' => [
-          ['Small Kitchen Appliances'],
-          ['Large Appliances'],         
-          ['Cooling & Heating'],      
-          ['Irons & Garment Steamers'],      
-          ['Vacuums & Floor Care'],      
-          ['Kitchen Appliances'],      
+          [
+            'Small Kitchen Appliances',
+            'child' => [
+                ['Food Preparation'],
+                ['Coffee Machines & Accessories'],
+                ['Electric Kettles & Thermo Pots'],
+                ['Juicers & Fruit Extractors'],
+                ['Specialty Cookware'],
+                ['Electric Woks'],
+                ['Suki Pots'],
+                ['Toasters'],
+                ['Electric Slow Cookers'],
+                ['Electric Food Steamers'],
+                ['Electric Pans'],
+                ['Electric Multi cookers'],
+                ['Electric Contact Grills'],
+                ['Electric Sandwich Makers & Presses'],
+                ['Rice Cookers'],
+                ['Electric Pressure cookers'],
+                ['Electric Knives & Sharpeners'],
+                ['Fryers'],
+            ]
+          ],
+          [
+            'Large Appliances',
+            'child' => [
+                ['Dish Washers & Dryers'],
+                ['Electric Grills'],
+                ['Freezers'],
+                ['Hoods'],
+                ['Cooktops'],
+                ['Microwaves & Ovens'],
+                ['Ranges & Hobs'],
+                ['Refrigerators'],
+                ['Washers & Dryers'],
+            ]
+          ],         
+          [
+            'Cooling & Heating',
+            'child' => [
+                ['Air Conditioning'],
+                ['Air Treatment'],
+                ['Fans'],
+                ['Heaters'],
+                ['Water Heaters'],
+                ['Exhaust Fans'],
+            ]
+          ],      
+          [
+            'Irons & Garment Steamers',
+            'child' => [
+                ['Irons'],
+                ['Garment Steamers'],
+                ['Garment Presses'],
+            ]
+          ],      
+          [
+            'Vacuums & Floor Care',
+            'child' => [
+              ['Vacuum Cleaners & Accessories'],
+              ['Steam Mops'],
+              ['Electric Brooms'],
+              ['Floor Polishers'],
+            ]
+          ],      
+          [
+            'Kitchen Appliances',
+            'child' => [
+                ['Refrigerators'],
+                ['Freezers'],
+                ['Wine Cellars'],
+                ['Dishwashers & Parts'],
+                ['Warming Drawers'],
+                ['Cooktops & Ranges'],
+                ['Microwaves & Parts'],
+                ['Ovens'],
+                ['Range Hoods'],
+                ['Water Dispensers, Purifiers & Filters'],
+                ['Gas Stoves'],
+            ]
+          ],      
           ['Sewing Machines'],     
-          ['Washers & Dryers'],      
-          ['Appliances Parts & Accessories'],                
+          [
+            'Washers & Dryers',
+            'child' => [
+                ['Washing Machines'],
+                ['Combo Washer Dryer'],
+                ['Clothes Dryers'],
+            ]
+          ],      
+          [
+            'Appliances Parts & Accessories',
+            'child' => [
+                ['Air Conditioner Accessories'],
+                ['Air Purifier Accessories'],
+                ['Fan Parts & Accessories'],
+                ['Water Heater Parts & Accessories'],
+                ['Ventilation Parts & Accessories'],
+                ['Sewing Machine Parts & Accessories'],
+                ['Vacuum Cleaners Accessories'],
+                ['Dishwasher Parts & Accessories'],
+                ['Freezer Parts & Accessories'],
+                ['Range Hood Parts & Accessories'],
+                ['Cooktop Parts & Accessories'],
+                ['Microwave Parts & Accessories'],
+                ['Wall Oven Parts & Accessories'],
+                ['Gas Stove Parts & Accessories'],
+                ['Refrigerator Parts & Accessories'],
+                ['Washer & Dryer Parts & Accessories'],
+                ['Small Kitchen Appliance Parts & Accessories'],
+                ['Coffee Machine Accessories'],
+                ['Rice Cookers and Steamers Accessories'],
+                ['Water Dispenser Accessories'],
+                ['Water Purifiers & Filters Accessories'],
+                ['Fryer and Air Fryer Accessories'],
+                ['Toasters and Sandwich Makers Accessories'],
+                ['Specialty Cookware Accessories'],
+            ]
+          ],                
       ]         
   ],
 ];
