2f16953b184d1542160e3ad20e1ce82a9883a08a
Rupa Paik <rupapaik@gmail.com>
2020-06-28 11:09:42 +0600

    Collect second child on camera categories and seed data

--- database/seeds/CategoriesAttr/Camera.php
+++ database/seeds/CategoriesAttr/Camera.php
@@ -5,21 +5,109 @@ $camera = [
 	[
         'Camera',
         'child' => [
-           
-                  ['Point & Shoot'],
-                  ['Bridge'],
-                  ['DSLR'],
-                  ['Instant Camera'],
+                  [
+                    'Point & Shoot',
+										  'child' => [
+											['Point & Shoots'],
+											['Underwater Digital Cameras'],
+										]
+									],
+                  [
+										'Bridge',],
+
+
+                  [
+										'DSLR',
+										'child' => [
+										['Sets'],
+										['Body Only'],
+									],
+                  [
+										'Instant Camera',
+										'child' => [
+										['Instant Cameras'],
+										['Instant Camera Films'],
+										['Instant Camera Accessories'],
+									]
+									],
                   ['Mirrorless'],
-                  ['Gadgets & Other Cameras'],
-                  ['Video & Action Camcorder'],
-                  ['Camera Accessories'],
+                  [
+										'Gadgets & Other Cameras',
+										'child' => [
+										['Instant Cameras'],
+										['Instant Camera Films'],
+										['Instant Camera Accessories'],
+									]
+									],
+                  [
+                    'Video & Action Camcorder',
+                    'child' => [
+                      ['Sports & Action Camera'],
+                      ['Video Camera'],
+                      ['Professional Video Camera'],
+                      ['Sports & Action Camera Accessories'],
+                      ['360 Cameras'],
+                     ]
+                  ],
+                  [
+                    'Camera Accessories',
+                    'child' => [
+                        ['Batteries'],
+                        ['Battery Chargers'],
+                        ['Camera Cases Covers and Bags'],
+                        ['Camera Remote Controls'],
+                        ['Dry Box'],
+                        ['Flashes'],
+                        ['Lighting & Studio Equipment'],
+                        ['Memory Cards'],
+                        ['Straps'],
+                        ['Tripods & Monopods'],
+                        ['Sports & Action Camera Accessories'],
+                        ['Camera Screen Protector'],
+                        ['Viewfinders'],
+                        ['Gimbals & Stabilizers'],
+                        ['Gimbals & Stabilizers Accessories'],
+                    ]
+                  ],
                   ['Gopro Model'],
-                  ['Optics'],
-                  ['Lenses'],
+                  [
+                    'Optics',
+                    'child' => [
+                      ['Monoculars'],
+                      ['Telescopes'],
+                      ['Binoculars'],
+                      ['Optics Accessories'],
+                    ]
+                  ],
+                  [
+                    'Lenses',
+                    'child' => [
+                      ['DSLRs Lenses'],
+                      ['Mirrorless Lenses'],
+                      ['Lens Accessories'],
+                      ['Other Lenses'],
+                      ['Smartphone Lenses'],
+                    ]
+                  ],
                   ['Car Cameras'],
-                  ['Security Cameras & Systems'],
-                  ['Drones'],        
-      ],
-  ],
+                  [
+										'Security Cameras & Systems',
+										  'child' => [
+											['CCTV Security Cameras'],
+											['IP Security Systems'],
+											['CCTV Security Systems'],
+											['IP Security Cameras'],
+											['Dummy Cameras'],
+										]
+									],
+                  [
+                    'Drones',
+                    'child' => [
+                      ['Drones'],
+                      ['Drone Accessories'],
+                    ]
+                  ],
+         ],
+     ]
+   ],
 ];
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
                 DB::statement('SET FOREIGN_KEY_CHECKS=0');
                 DB::table('customers')->truncate();
-                DB::table('categories')->truncate();
+                // DB::table('categories')->truncate();
                 DB::table('couriers')->truncate();
                 DB::table('currencies')->truncate();
                 DB::table('promotion_heads')->truncate();
@@ -35,10 +35,10 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
                 RoleTableSeeder::class,
-                CustomersTableSeeder::class,
+                // CustomersTableSeeder::class,
                 PermissionTableSeeder::class,
                 UserTableSeeder::class,
-               // CategoriesTableSeeder::class,
+                CategoriesTableSeeder::class,
                 CuriersTableSeeder::class,
                 CurrenciesTableSeeder::class,
                 PromotionHeadsTableSeeder::class,
