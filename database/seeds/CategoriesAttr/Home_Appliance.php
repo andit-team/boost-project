6fdb50bfbff71224ce205197fd02a7ac4ff2b69b
Rupa Paik <rupapaik@gmail.com>
2020-06-28 14:19:04 +0600

    Collect first child on home appliance

---
+++ database/seeds/CategoriesAttr/Home_Appliance.php
@@ -0,0 +1,19 @@
+<?php
+//multi-select,select,text,checkbox,number
+
+$home_appliances = [
+	[
+        'Home Appliance',
+        'child' => [
+          ['Small Kitchen Appliances'],
+          ['Large Appliances'],         
+          ['Cooling & Heating'],      
+          ['Irons & Garment Steamers'],      
+          ['Vacuums & Floor Care'],      
+          ['Kitchen Appliances'],      
+          ['Sewing Machines'],     
+          ['Washers & Dryers'],      
+          ['Appliances Parts & Accessories'],                
+      ]         
+  ],
+];
--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -14,19 +14,26 @@ class CategoriesTableSeeder extends Seeder
     include('CategoriesAttr/Computers_Laptop.php');
     // include('CategoriesAttr/TV_Audio.php');
     include('CategoriesAttr/Camera.php');
-    // dd($camera);
     include('CategoriesAttr/tv_audio_video_gaming.php');
+    include('CategoriesAttr/Home_Appliance.php');
 
 		// dd($mobiles_tablets);
 
       \Baazar::insertRecords($mobiles_tablets);
       echo  'Mobiles & Tablets Done...<>';
+
       \Baazar::insertRecords($Computers_Laptop);
       echo  'Computers & Laptops Done....';
+
       \Baazar::insertRecords($tv_audio_video_gaming);
       echo  'TV, Audio , Video, Gaming Done...';
+
       \Baazar::insertRecords($camera);
       echo  'Camera Done....';
+
+      \Baazar::insertRecords($home_appliances);
+      echo  'Home Appliance Done....';
+
       // \Baazar::insertRecords($tv_audio);
       // echo  'TV, Audio  Done...';
    	}
