5293e0d29832ee4a7c02da8d1696a6bb244aaa61
Shovon Das <shovondas26@gmail.com>
2020-07-19 12:48:34 +0600

    fix

--- routes/agent.php
+++ routes/agent.php
@@ -3,8 +3,8 @@
 /***************
  * Registration
  ***************/
-Route::get('agent/login','agentController@agentlogin');
-Route::post('agent/login','agentController@agentloginprocess')->name('agentloginprocess');
+Route::get('agent-registration','MerchantController@agentRegistration');
+Route::post('agent-registration','MerchantController@registrationStepOne')->name('profileRegistration');
 
 /***************
  * Login
