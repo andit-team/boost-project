a251d77692964187ff5b22a56955b06a0fb10041
Shovon Das <shovondas26@gmail.com>
2020-07-19 11:46:35 +0600

    comment style

--- routes/agent.php
+++ routes/agent.php
@@ -1,13 +1,13 @@
 <?php
 
-/**
+/***************
  * Registration
- */
+ ***************/
 Route::get('agent/login','agentController@agentlogin');
 Route::post('agent/login','agentController@agentloginprocess')->name('agentloginprocess');
 
-/**
+/***************
  * Login
- */
+ ***************/
 Route::get('agent/login','agentController@agentlogin');
 Route::post('agent/login','agentController@agentloginprocess')->name('agentloginprocess');
